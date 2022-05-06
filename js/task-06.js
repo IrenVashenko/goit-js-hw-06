acac0f0b7f7b0de5b85ac7451dbb65224efa845d
Ira <vashenko1607@gmail.com>
2022-05-06 13:14:41 +0300

    added changes

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,8 @@
 const textInput = document.querySelector('input');
 const textInputLength = document.querySelector('input[data-length="6"]').dataset.length;
-console.log(Number(textInputLength))
 
-textInput.addEventListener("input", (event) => {
+
+textInput.addEventListener("blur", (event) => {
     if (event.currentTarget.value.length === Number(textInputLength)) {
         textInput.classList.replace('invalid', 'valid')
     } else {
